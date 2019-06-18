67819c30eb679df2f8c01d5db59014229cdb448c
Stepaska <51706288+Stepaska@users.noreply.github.com>
2019-06-18 20:33:07 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-alert("Текст")
+function humburgerKlick() {
+    var mobileMenu = document.getElementsByClassName("mobile-menu")[0];
+   var display = mobileMenu.style.display;
+   if (display == "") {
+    mobileMenu.style.display = "flex"
+   }else{
+    mobileMenu.style.display = "";
+   }
+
+}
+
+function moreKlick() {
+    var mobileMenu = document.getElementsByClassName("item-window")[0];
+   var display = mobileMenu.style.display;
+   if (display == "") {
+    mobileMenu.style.display = "flex"
+   }else{
+    mobileMenu.style.display = "";
+   }
+
+}
+
+var humburgerIcon = document.getElementsByClassName("menu-icon") [0];
+humburgerIcon.addEventListener("click", humburgerKlick ) ;
+
+
+var moreIcon = document.getElementsByClassName("card-button") [1];
+moreIcon.addEventListener("click", moreKlick ) ;
+
+var a = document.getElementsByClassName("item-close")[0];
+a.addEventListener("click", moreKlick ) ;
